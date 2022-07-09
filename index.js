91d5736ece2260daeeb7216275c9f29a2b637316
jamaldizaji <jdizaji3@gmail.com>
2022-07-09 23:44:00 +0430

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+console.log(process.env.PORT);
+console.log(process.env.BASE_URL);
+console.log(process.env.PORT);
+console.log(process.env.PORT);
+console.log(process.env.PORT);
